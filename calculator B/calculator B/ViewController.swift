00974e92d159f2c3545fdbc4bf2eec41012b003d
Mohamed Eltaher <mohamed.ahmedeltaher@gmail.com>
2020-06-22 13:38:08 +0200

    new

--- calculator B/calculator B/ViewController.swift
+++ calculator B/calculator B/ViewController.swift
@@ -18,40 +18,54 @@ class ViewController: UIViewController {
     //defual value ((will use this code in clear button ))
     @IBOutlet weak var label1: UILabel!
     var labelstring = "0"
-    var savenum = 0
+    var savedNum = 0
     var lastbuttonpressedwasmode = false
     var currentmode :modes = .notSet
    
     
-    func updatetext (){
-        guard let labelint = Int(labelstring)  else {
-            label1.text = "error"
-            return
-        }
-        if currentmode == .notSet {
-            savenum = labelint
-        }
-        let formatter: NumberFormatter = NumberFormatter()
-               formatter.numberStyle = .decimal
-               let num: NSNumber = NSNumber(value: labelint)
-               
-               label1.text = formatter.string(from: num)
-    }
+    func updateText() {
+           guard let labelInt: Int = Int(labelstring) else {
+               label1.text = "Error"
+               return
+           }
+           
+           if currentmode == .notSet {
+               savedNum = labelInt
+           }
+           
+          let formatter: NumberFormatter = NumberFormatter()
+                 formatter.numberStyle = .decimal
+                 let num: NSNumber = NSNumber(value: labelInt)
+                 
+                 label1.text = formatter.string(from: num)
+       }
     func changemode (mode : modes){
         currentmode = mode
         lastbuttonpressedwasmode = true
     }
     
     @IBAction func equal(_ sender: Any) {
+        guard let labelint: Int = Int(labelstring) else {
+                   label1.text = "Error"
+                   return
+               }
+               if currentmode == .notSet || lastbuttonpressedwasmode {
+                   return
+               }
+               
+               if currentmode == .addition {
+                   savedNum += labelint
+               } else if currentmode == .subtraction {
+                   savedNum -= labelint
+               }else if currentmode == .division {
+                savedNum /= labelint }
+               else if currentmode == .muliplication {
+                savedNum *= labelint }
         
-        
-        
-        
-        
-        labelstring = "0"
-              savenum = 0
-              lastbuttonpressedwasmode = false
-              currentmode = .notSet
+        currentmode = .notSet
+        labelstring = "\(savedNum) "
+        updateText()
+        lastbuttonpressedwasmode = false
 
     }
     @IBAction func addition(_ sender: Any) {
@@ -68,23 +82,25 @@ class ViewController: UIViewController {
     }
     @IBAction func clearbutton(_ sender: Any) {
        labelstring = "0"
-       savenum = 0
+       savedNum = 0
        lastbuttonpressedwasmode = false
        currentmode = .notSet
         
     }
     
-    @IBAction func didpressbutton(_ sender: UIButton) {
-        guard let anothervalue : String = sender.titleLabel?.text
-            else {
-                label1.text = "error"
-                return   }
-        if lastbuttonpressedwasmode == true {
-            lastbuttonpressedwasmode = false
-            labelstring = " 0"
-        }
-        labelstring = labelstring.appending(anothervalue)
-        updatetext()
+    @IBAction func didpressnumber(_ sender: UIButton) {
+       guard let stringValue: String = sender.titleLabel?.text else {
+                  label1.text = "Error"
+                  return
+              }
+              
+              if lastbuttonpressedwasmode {
+                  lastbuttonpressedwasmode = false
+                  labelstring = "0"
+              }
+              
+              labelstring = labelstring.appending(stringValue)
+              updateText()
 }
 
 }
